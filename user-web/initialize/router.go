c6b131d5c9b7fd4fc1bbd5a30d96a8a5d4b4b351
zidon <403907871@qq.com>
2021-10-06 17:55:26 +0800

    - 增加跨域处理

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -2,11 +2,13 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"mxshop_api/user-web/middlewares"
 	"mxshop_api/user-web/router"
 )
 
 func Routers() *gin.Engine {
 	r := gin.Default()
+	r.Use(middlewares.Cors())
 
 	ApiGroup := r.Group("/u/v1")
 	router.InitUserRouter(ApiGroup)
